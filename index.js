008d7858d49dc3d8925cac220cc3a97188b3a910
Amit <amit1999ghosh99@gmail.com>
2021-03-25 11:34:05 +0530

    database connected

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const Port = process.env.PORT || 1000;
 const mongoose = require('mongoose');
 const userRoute = require("./routes/user")
-// mongoose.connect("mongodb+srv://mainbackend:<password>TdO@mainbackend.i3efy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-// });
+mongoose.connect("mongodb+srv://mainbackend:nOgzMsTys79DtXpr@mainbackend.i3efy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("Mongoose Connect successfully")
@@ -27,3 +27,5 @@ app.use("/user", userRoute);
 
 
 app.listen(Port, () => console.log(`Your server is running on port ${Port}`))
+
+//nOgzMsTys79DtXpr
